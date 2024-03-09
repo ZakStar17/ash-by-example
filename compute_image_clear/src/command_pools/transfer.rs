d4d4d76f0c7a5bcd7ef8521cc0d02deb64c01ff0
Zakhar Starinski <zakharstarinski17@gmail.com>
2024-03-09 04:20:12 +0000

    Updated image clear to have better error handling and be more useful

--- compute_image_clear/src/command_pools/transfer.rs
+++ compute_image_clear/src/command_pools/transfer.rs
@@ -2,7 +2,9 @@ use std::ptr;
 
 use ash::vk;
 
-use crate::{device::QueueFamilies, IMAGE_HEIGHT, IMAGE_WIDTH};
+use crate::{device::QueueFamilies, errors::OutOfMemoryError, IMAGE_HEIGHT, IMAGE_WIDTH};
+
+use super::dependency_info;
 
 pub struct TransferCommandBufferPool {
   pool: vk::CommandPool,
@@ -10,41 +12,34 @@ pub struct TransferCommandBufferPool {
 }
 
 impl TransferCommandBufferPool {
-  pub fn create(device: &ash::Device, queue_families: &QueueFamilies) -> Self {
+  pub fn create(device: &ash::Device, queue_families: &QueueFamilies) -> Result<Self, vk::Result> {
     let flags = vk::CommandPoolCreateFlags::TRANSIENT;
-    let pool = super::create_command_pool(device, flags, queue_families.get_transfer_index());
+    let pool = super::create_command_pool(device, flags, queue_families.get_transfer_index())?;
 
-    let copy_to_host = super::allocate_primary_command_buffers(device, pool, 1)[0];
+    let copy_to_host = super::allocate_primary_command_buffers(device, pool, 1)?[0];
 
-    Self { pool, copy_to_host }
+    Ok(Self { pool, copy_to_host })
   }
 
-  pub unsafe fn reset(&mut self, device: &ash::Device) {
-    device
-      .reset_command_pool(self.pool, vk::CommandPoolResetFlags::empty())
-      .expect("Failed to reset command pool");
+  pub unsafe fn reset(&mut self, device: &ash::Device) -> Result<(), vk::Result> {
+    device.reset_command_pool(self.pool, vk::CommandPoolResetFlags::empty())
   }
 
-  pub unsafe fn destroy_self(&mut self, device: &ash::Device) {
-    device.destroy_command_pool(self.pool, None);
-  }
-
-  pub unsafe fn record_copy_img_to_host(
+  pub unsafe fn record_copy_img_to_buffer(
     &mut self,
     device: &ash::Device,
     queue_families: &QueueFamilies,
     src_image: vk::Image,
-    dst_image: vk::Image,
-  ) {
+    dst_buffer: vk::Buffer,
+  ) -> Result<(), OutOfMemoryError> {
+    let cb = self.copy_to_host;
     let begin_info = vk::CommandBufferBeginInfo {
       s_type: vk::StructureType::COMMAND_BUFFER_BEGIN_INFO,
       p_next: ptr::null(),
       flags: vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT,
       p_inheritance_info: ptr::null(),
     };
-    device
-      .begin_command_buffer(self.copy_to_host, &begin_info)
-      .expect("Failed to begin recording command buffer");
+    device.begin_command_buffer(cb, &begin_info)?;
 
     let subresource_range = vk::ImageSubresourceRange {
       aspect_mask: vk::ImageAspectFlags::COLOR,
@@ -54,48 +49,22 @@ impl TransferCommandBufferPool {
       layer_count: 1,
     };
 
-    // This is the matching queue family ownership acquire operation to the one in the compute
-    // command buffer which executed on the source image
-    let src_acquire = vk::ImageMemoryBarrier {
-      s_type: vk::StructureType::IMAGE_MEMORY_BARRIER,
+    // matches to release found in compute
+    let src_acquire = vk::ImageMemoryBarrier2 {
+      s_type: vk::StructureType::IMAGE_MEMORY_BARRIER_2,
       p_next: ptr::null(),
-
-      // should be NONE for ownership acquire
-      src_access_mask: vk::AccessFlags::NONE,
-      // change image AccessFlags after the ownership transfer completes
-      dst_access_mask: vk::AccessFlags::TRANSFER_READ,
-
-      // should match the layouts specified in the compute buffer
+      src_access_mask: vk::AccessFlags2::NONE, // NONE for ownership acquire,
+      dst_access_mask: vk::AccessFlags2::TRANSFER_READ,
+      src_stage_mask: vk::PipelineStageFlags2::TRANSFER, // from semaphore
+      dst_stage_mask: vk::PipelineStageFlags2::COPY,
       old_layout: vk::ImageLayout::TRANSFER_DST_OPTIMAL,
       new_layout: vk::ImageLayout::TRANSFER_SRC_OPTIMAL,
-
       src_queue_family_index: queue_families.get_compute_index(),
       dst_queue_family_index: queue_families.get_transfer_index(),
       image: src_image,
       subresource_range,
     };
-    // change destination image layout and access flags to transfer write
-    let dst_transfer_dst_layout = vk::ImageMemoryBarrier {
-      s_type: vk::StructureType::IMAGE_MEMORY_BARRIER,
-      p_next: ptr::null(),
-      src_access_mask: vk::AccessFlags::empty(),
-      dst_access_mask: vk::AccessFlags::TRANSFER_WRITE,
-      old_layout: vk::ImageLayout::UNDEFINED,
-      new_layout: vk::ImageLayout::TRANSFER_DST_OPTIMAL,
-      src_queue_family_index: vk::QUEUE_FAMILY_IGNORED,
-      dst_queue_family_index: vk::QUEUE_FAMILY_IGNORED,
-      image: dst_image,
-      subresource_range,
-    };
-    device.cmd_pipeline_barrier(
-      self.copy_to_host,
-      vk::PipelineStageFlags::TRANSFER,
-      vk::PipelineStageFlags::TRANSFER,
-      vk::DependencyFlags::empty(),
-      &[],
-      &[],
-      &[src_acquire, dst_transfer_dst_layout],
-    );
+    device.cmd_pipeline_barrier2(cb, &dependency_info(&[], &[], &[src_acquire]));
 
     // 1 color layer
     let subresource_layers = vk::ImageSubresourceLayers {
@@ -105,53 +74,48 @@ impl TransferCommandBufferPool {
       layer_count: 1,
     };
     // full image
-    let copy_region = vk::ImageCopy {
-      src_subresource: subresource_layers,
-      src_offset: vk::Offset3D { x: 0, y: 0, z: 0 },
-      dst_subresource: subresource_layers,
-      dst_offset: vk::Offset3D { x: 0, y: 0, z: 0 },
-      extent: vk::Extent3D {
+    let copy_region = vk::BufferImageCopy {
+      image_subresource: subresource_layers,
+      image_offset: vk::Offset3D { x: 0, y: 0, z: 0 },
+      image_extent: vk::Extent3D {
         width: IMAGE_WIDTH,
         height: IMAGE_HEIGHT,
         depth: 1,
       },
+      buffer_offset: 0,
+      buffer_image_height: 0, // densely packed
+      buffer_row_length: 0,
     };
-    device.cmd_copy_image(
-      self.copy_to_host,
+    device.cmd_copy_image_to_buffer(
+      cb,
       src_image,
       vk::ImageLayout::TRANSFER_SRC_OPTIMAL,
-      dst_image,
-      vk::ImageLayout::TRANSFER_DST_OPTIMAL,
+      dst_buffer,
       &[copy_region],
     );
 
-    // change destination image access flags to host read
-    let make_dst_host_accessible = vk::ImageMemoryBarrier {
-      s_type: vk::StructureType::IMAGE_MEMORY_BARRIER,
+    // flush memory to host
+    let flush_host = vk::BufferMemoryBarrier2 {
+      s_type: vk::StructureType::BUFFER_MEMORY_BARRIER_2,
       p_next: ptr::null(),
-      src_access_mask: vk::AccessFlags::TRANSFER_WRITE,
-      dst_access_mask: vk::AccessFlags::HOST_READ,
-      old_layout: vk::ImageLayout::TRANSFER_DST_OPTIMAL,
-      // Optimal layouts can have different internal representations depending on what the driver
-      // implemented, GENERAL must be used in order to interpret the image by the CPU
-      new_layout: vk::ImageLayout::GENERAL,
+      src_access_mask: vk::AccessFlags2::TRANSFER_WRITE,
+      dst_access_mask: vk::AccessFlags2::HOST_READ,
+      src_stage_mask: vk::PipelineStageFlags2::COPY,
+      dst_stage_mask: vk::PipelineStageFlags2::HOST,
       src_queue_family_index: vk::QUEUE_FAMILY_IGNORED,
       dst_queue_family_index: vk::QUEUE_FAMILY_IGNORED,
-      image: dst_image,
-      subresource_range,
+      buffer: dst_buffer,
+      offset: 0,
+      size: vk::WHOLE_SIZE,
     };
-    device.cmd_pipeline_barrier(
-      self.copy_to_host,
-      vk::PipelineStageFlags::TRANSFER,
-      vk::PipelineStageFlags::HOST,
-      vk::DependencyFlags::empty(),
-      &[],
-      &[],
-      &[make_dst_host_accessible],
-    );
+    device.cmd_pipeline_barrier2(cb, &dependency_info(&[], &[flush_host], &[]));
 
-    device
-      .end_command_buffer(self.copy_to_host)
-      .expect("Failed to finish recording command buffer");
+    device.end_command_buffer(cb)?;
+
+    Ok(())
+  }
+
+  pub unsafe fn destroy_self(&mut self, device: &ash::Device) {
+    device.destroy_command_pool(self.pool, None);
   }
 }
--- compute_image_clear/src/main.rs
+++ compute_image_clear/src/main.rs
@@ -1,8 +1,10 @@
+mod allocator;
 mod command_pools;
 mod device;
 mod entry;
-mod image;
+mod errors;
 mod instance;
+mod renderer;
 mod utility;
 
 // validation layers module will only exist if validation layers are enabled
@@ -10,198 +12,64 @@ mod utility;
 mod validation_layers;
 
 use ash::vk;
-use command_pools::{ComputeCommandBufferPool, TransferCommandBufferPool};
-use device::PhysicalDevice;
-use image::Image;
-use std::{
-  ffi::CStr,
-  ops::BitOr,
-  ptr::{self, addr_of},
-};
-use utility::cstr;
+use std::ffi::CStr;
 
-// array of validation layers that should be loaded
+use crate::renderer::Renderer;
+
 // validation layers names should be valid cstrings (not contain null bytes nor invalid characters)
 #[cfg(feature = "vl")]
-pub const VALIDATION_LAYERS: [&'static CStr; 1] = [cstr!("VK_LAYER_KHRONOS_validation")];
+const VALIDATION_LAYERS: [&'static CStr; 1] = [cstr!("VK_LAYER_KHRONOS_validation")];
 #[cfg(feature = "vl")]
-pub const ADDITIONAL_VALIDATION_FEATURES: [vk::ValidationFeatureEnableEXT; 2] = [
+const ADDITIONAL_VALIDATION_FEATURES: [vk::ValidationFeatureEnableEXT; 2] = [
   vk::ValidationFeatureEnableEXT::BEST_PRACTICES,
   vk::ValidationFeatureEnableEXT::SYNCHRONIZATION_VALIDATION,
 ];
 
-pub const TARGET_API_VERSION: u32 = vk::API_VERSION_1_3;
+const TARGET_API_VERSION: u32 = vk::API_VERSION_1_3;
 
-pub const APPLICATION_NAME: &'static CStr = cstr!("Image clear");
-pub const APPLICATION_VERSION: u32 = vk::make_api_version(0, 1, 0, 0);
+const APPLICATION_NAME: &'static CStr = cstr!("Image clear");
+const APPLICATION_VERSION: u32 = vk::make_api_version(0, 1, 0, 0);
 
-pub const REQUIRED_DEVICE_EXTENSIONS: [&'static CStr; 0] = [];
+const REQUIRED_DEVICE_EXTENSIONS: [&'static CStr; 0] = [];
 
-pub const IMAGE_WIDTH: u32 = 1920;
-pub const IMAGE_HEIGHT: u32 = 1080;
+const IMAGE_WIDTH: u32 = 1920;
+const IMAGE_HEIGHT: u32 = 1080;
 
-// device selection checks if format is available
-pub const IMAGE_FORMAT: vk::Format = vk::Format::R8G8B8A8_UINT;
-pub const IMAGE_SAVE_TYPE: ::image::ColorType = ::image::ColorType::Rgba8; // should be equivalent
-                                                                           // valid color values depend on IMAGE_FORMAT
-pub const IMAGE_COLOR: vk::ClearColorValue = vk::ClearColorValue {
-  uint32: [134, 206, 203, 255], // rgba(134, 206, 203, 255)
+const IMAGE_FORMAT: vk::Format = vk::Format::R8G8B8A8_UNORM;
+const IMAGE_FORMAT_SIZE: u64 = 4;
+const IMAGE_MINIMAL_SIZE: u64 = IMAGE_WIDTH as u64 * IMAGE_HEIGHT as u64 * IMAGE_FORMAT_SIZE;
+
+const IMAGE_SAVE_TYPE: image::ColorType = image::ColorType::Rgba8; // should be equivalent
+                                                                   // valid color values depend on IMAGE_FORMAT
+const IMAGE_COLOR: vk::ClearColorValue = vk::ClearColorValue {
+  float32: [134.0 / 255.0, 206.0 / 255.0, 203.0 / 255.0, 1.0], // rgba(134, 206, 203, 255)
 };
 
 const IMAGE_SAVE_PATH: &str = "image.png";
 
-fn create_semaphore(device: &ash::Device) -> vk::Semaphore {
-  let create_info = vk::SemaphoreCreateInfo {
-    s_type: vk::StructureType::SEMAPHORE_CREATE_INFO,
-    p_next: ptr::null(),
-    flags: vk::SemaphoreCreateFlags::empty(),
-  };
-  unsafe {
-    device
-      .create_semaphore(&create_info, None)
-      .expect("Failed to create a semaphore")
-  }
-}
-
-fn create_fence(device: &ash::Device) -> vk::Fence {
-  let create_info = vk::FenceCreateInfo {
-    s_type: vk::StructureType::FENCE_CREATE_INFO,
-    p_next: ptr::null(),
-    flags: vk::FenceCreateFlags::empty(),
-  };
-  unsafe {
-    device
-      .create_fence(&create_info, None)
-      .expect("Failed to create a fence")
-  }
-}
-
 fn main() {
   env_logger::init();
 
-  let entry: ash::Entry = unsafe { entry::get_entry() };
-
-  #[cfg(feature = "vl")]
-  let (instance, mut debug_utils) = instance::create_instance(&entry);
-  #[cfg(not(feature = "vl"))]
-  let instance = instance::create_instance(&entry);
-
-  let physical_device = unsafe { PhysicalDevice::select(&instance) };
-
-  let (device, queues) = device::create_logical_device(&instance, &physical_device);
-
-  println!("Allocating images...");
-  // GPU image with DEVICE_LOCAL flags
-  let mut local_image = Image::new(
-    &device,
-    &physical_device,
-    vk::ImageTiling::OPTIMAL,
-    vk::ImageUsageFlags::TRANSFER_SRC.bitor(vk::ImageUsageFlags::TRANSFER_DST),
-    vk::MemoryPropertyFlags::DEVICE_LOCAL,
-    vk::MemoryPropertyFlags::empty(),
-  );
-  // CPU accessible image with HOST_VISIBLE flags
-  let mut host_image = Image::new(
-    &device,
-    &physical_device,
-    vk::ImageTiling::LINEAR,
-    vk::ImageUsageFlags::TRANSFER_DST,
-    vk::MemoryPropertyFlags::HOST_VISIBLE,
-    vk::MemoryPropertyFlags::HOST_CACHED,
-  );
-
-  let mut compute_pool = ComputeCommandBufferPool::create(&device, &physical_device.queue_families);
-  let mut transfer_pool =
-    TransferCommandBufferPool::create(&device, &physical_device.queue_families);
-
-  // record command buffers
-  unsafe {
-    compute_pool.reset(&device);
-    compute_pool.record_clear_img(&device, &physical_device.queue_families, *local_image);
-
-    transfer_pool.reset(&device);
-    transfer_pool.record_copy_img_to_host(
-      &device,
-      &physical_device.queue_families,
-      *local_image,
-      *host_image,
-    );
-  }
-
-  let image_clear_finished = create_semaphore(&device);
-  let clear_image_submit = vk::SubmitInfo {
-    s_type: vk::StructureType::SUBMIT_INFO,
-    p_next: ptr::null(),
-    wait_semaphore_count: 0,
-    p_wait_semaphores: ptr::null(),
-    p_wait_dst_stage_mask: ptr::null(),
-    command_buffer_count: 1,
-    p_command_buffers: addr_of!(compute_pool.clear_img),
-    signal_semaphore_count: 1,
-    p_signal_semaphores: addr_of!(image_clear_finished),
-  };
-  let wait_for = vk::PipelineStageFlags::TRANSFER;
-  let transfer_image_submit = vk::SubmitInfo {
-    s_type: vk::StructureType::SUBMIT_INFO,
-    p_next: ptr::null(),
-    wait_semaphore_count: 1,
-    p_wait_semaphores: addr_of!(image_clear_finished),
-    p_wait_dst_stage_mask: addr_of!(wait_for),
-    command_buffer_count: 1,
-    p_command_buffers: addr_of!(transfer_pool.copy_to_host),
-    signal_semaphore_count: 0,
-    p_signal_semaphores: ptr::null(),
-  };
-
-  let finished = create_fence(&device);
+  let mut renderer = Renderer::initialize(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_MINIMAL_SIZE).expect("Failed to initialize");
+  unsafe { renderer.record_work() }.expect("Failed to record work");
 
   println!("Submitting work...");
-  unsafe {
-    // note: you can make multiple submits with device.queue_submit2
-    device
-      .queue_submit(queues.compute, &[clear_image_submit], vk::Fence::null())
-      .expect("Failed to submit compute");
-    device
-      .queue_submit(queues.transfer, &[transfer_image_submit], finished)
-      .expect("Failed to submit transfer");
-
-    device
-      .wait_for_fences(&[finished], true, u64::MAX)
-      .expect("Failed to wait for fences");
-  }
+  renderer.submit_and_wait().expect("Failed to submit work");
   println!("GPU finished!");
 
   println!("Saving file...");
-  host_image.save_to_file(&device, &physical_device, IMAGE_SAVE_PATH);
-  println!("Done!");
-
-  // Cleanup
-  log::info!("Destroying and releasing resources");
   unsafe {
-    // wait until all operations have finished and the device is safe to destroy
-    device
-      .device_wait_idle()
-      .expect("Failed to wait for the device to become idle");
-
-    device.destroy_fence(finished, None);
-    device.destroy_semaphore(image_clear_finished, None);
-
-    compute_pool.destroy_self(&device);
-    transfer_pool.destroy_self(&device);
-
-    local_image.destroy_self(&device);
-    host_image.destroy_self(&device);
-
-    log::debug!("Destroying device");
-    device.destroy_device(None);
-
-    #[cfg(feature = "vl")]
-    {
-      log::debug!("Destroying debug utils messenger");
-      debug_utils.destroy_self();
-    }
-
-    log::debug!("Destroying Instance");
-    instance.destroy_instance(None);
+    renderer.get_resulting_data(|data| {
+      image::save_buffer(
+        IMAGE_SAVE_PATH,
+        data,
+        IMAGE_WIDTH,
+        IMAGE_HEIGHT,
+        IMAGE_SAVE_TYPE,
+      )
+      .expect("Failed to save image");
+    })
   }
+  .expect("Failed to get resulting data");
+  println!("Done!");
 }
